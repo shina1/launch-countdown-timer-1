d5c6503aed8ec6146066d33845b7cc6becd0b356
DevNaftan <me@naftan.dev>
2020-12-29 14:29:45 -0500

    CSS Optimization

--- js/index.js
+++ js/index.js
@@ -24,15 +24,17 @@
       countdownValue.seconds -= 1
       seconds = countdownValue.seconds < 10 ? `0${countdownValue.seconds}` : `${countdownValue.seconds}`;
       target.seconds.querySelector('.card-face-back').textContent = seconds;
-      target.seconds.querySelector('.base').dataset.baseBefore = seconds;
+      target.seconds.dataset.baseAfter = seconds;
       target.seconds.querySelector('.card').classList.add('flip');
       target.seconds.querySelector('.card').addEventListener('animationend', event => {
         if(event.animationName === 'flip') {
           target.seconds.querySelector('.card-face-front').textContent = seconds;
-          target.seconds.querySelector('.base').dataset.baseAfter = seconds;
+          target.seconds.dataset.baseBefore = seconds;
           target.seconds.querySelector('.card').classList.remove('flip');
         }
       })
+    } else {
+      countdownValue.seconds = 59;
     }
   }
 
